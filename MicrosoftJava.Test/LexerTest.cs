ee91abe7008f8191db0d934c889e5980d1a61465
Vasil Vasilev <v.vasilev.12@ucl.ac.uk>
2013-03-10 11:37:57 +0000

    Modified CPPLexer

--- MicrosoftJava.Test/LexerTest.cs
+++ MicrosoftJava.Test/LexerTest.cs
@@ -7,11 +7,20 @@ namespace MicrosoftJava.Test {
     [TestClass]
     public class LexerTest {
         private readonly string CSharpTestPath = @"CSharpTestCode.cs";
+        private readonly string CTestPath = @"C:\Users\vasil.vasilev\Dropbox\UCL\Advanced Analysis and Design A\Project\klee-build-env\klee-uclibc-0.02-x64\libpthread\linuxthreads\sysdeps\mips\tls.h";
 
         [TestMethod]
         public void TestCSharpLexer() {
             var words = API.PublicAPI.ExtractTokens(new System.IO.StreamReader(CSharpTestPath), TLanguageType.CSharp);
             Assert.IsTrue(words.Count() > 0);
         }
+
+        [TestMethod]
+        public void TestCLexer() {
+            var words = API.PublicAPI.ExtractTokens(new System.IO.StreamReader(CTestPath), TLanguageType.C);
+            Assert.IsTrue(words.Count() > 0);
+        }
+     
+           
     }
 }
